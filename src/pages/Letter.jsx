b26f180c289699bb8452de090e5a2a7decc3ae99
unknown <aadityastha123456@gmail.com>
2025-02-12 22:46:51 +0545

    text changed

--- src/pages/Letter.jsx
+++ src/pages/Letter.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const message = "I want to tell you how much you mean to me...";
+const message = "";
 
 function Letter() {
   const [text, setText] = useState(
-    "Sorryyyy my princesss, timi eklo yesto manxey hau mero life ma jo sanga bolda ma harek moment happy hunxu, no matter what. I'm always waiting for you messages. You're always in my head. Timi aayesi I've been a lot more happy than I was. Never thought I would find someone who takes care of me like this (sayad garxau hola, ma bhram ma ni hunxa sakxu 😒). Aba mero side bata you're more than a friend to me. You mean soooooooo much to me. I'm always afraid ki timi ma bata bored hunxau ani you'll stop talking, insecurity at it's peak, tei ni aba k garammmm. K GARUUUU, ma k garuuuu. MAYALUUUUUUUUU, narisauuuuu naaaaa."
+    "Sorryyyy my princesss, timi eklo yesto manxey hau mero life ma jo sanga bolda ma harek moment happy hunxu, no matter what. I'm always waiting for you messages. You're always in my head. Timi aayesi I've been a lot more happier than I was. Never thought I would find someone who takes care of me like this (sayad garxau hola, ma bhram ma ni hunxa sakxu 😒). Aba mero side bata you're more than a friend to me. You mean soooooooo much to me. I'm always afraid ki timi ma bata bored hunxau ani you'll stop talking, insecurity at it's peak, tei ni aba k garammmm. K GARUUUU, ma k garuuuu. MAYALUUUUUUUUU, narisauuuuu naaaaa."
   );
   useEffect(() => {
     let i = 0;
